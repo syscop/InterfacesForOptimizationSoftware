715f0020769a9fc6292421c6769430b142f3bf85
Per Rutquist <per.rutquist@gmail.com>
2019-04-03 16:41:33 +0200

    Not really working, but commit anyways

--- PanelMatrices/src/PanelMatrices.jl
+++ PanelMatrices/src/PanelMatrices.jl
@@ -1,52 +1,83 @@
 module PanelMatrices
 
 using StaticNumbers
+using StaticArrays
+
+export PanelMatrix
 
 """
-PanelMatrix is panel-major, with column-major ordering of panels and within panels.
+A `PanelMatrix` is a panel-major matrix, or a view into a panel-major matrix.
+
+It contains a `.data` field which holds a matrix of tiles.
+
+Tiles can be, for example, of type `StaticMatrix`.
 """
-struct PanelMatrix{T,V,M,N,Q,R,O,P,U,V} <: AbstractMatrix{T}
-    data::V # (view on) the actual memory
-    eltype::Type{T} # type of the elements
+struct PanelMatrix{T,D,M,N,S,U,V,O,P,Q,R} <: AbstractMatrix{T}
+    data::D # the actual data (must be an AbstractMatrix{S} of panels)
+    element_type::Type{T} # type of the elements
     rows::StaticInteger{M} # rows in this matrix
     cols::StaticInteger{N} # columns in this matrix
-    rows_per_panel::StaticInteger{Q} # rows in a panel (rows in memory)
-    cols_per_panel::StaticInteger{R} # columns in a panel (columns in memory)
-    offset_row::StaticInteger{O} # starting row of the matrix (in memory)
-    offset_col::StaticInteger{P} # starting column of the matrix (in memory)
-    panel_rows::StaticInteger{U} # number of rows of panels
-    panel_cols::StaticInteger{V} # number of columns of panels
-    function PanelMatrix(data::AbstractArray{T}, rows, cols, rows_per_panel, cols_per_panel,
-                         offset_row=0, offset_col=0,
-                         panel_rows=ceil(Int, (rows+offset_row)//rows_per_panel),
-                         panel_cols=ceil(Int, (cols+offset_col)//cols_per_panel),
-                         ) where T
-        @boundscheck length(data) >= panel_rows*rows_per_panel*panel_cols*cols_per_panel || error("Too little data!")
-        new{data, T, }() #TODO
-    end
+    panel_type::Type{S} # Type of each panel
+    rows_per_panel::StaticInteger{U} # number of rows in a panel
+    cols_per_panel::StaticInteger{V} # number of columns in a panel
+    offset_row::StaticInteger{O} # starting row of the matrix
+    offset_col::StaticInteger{P} # starting column of the matrix
+    panel_rows::StaticInteger{Q} # number of rows of panels (can be larger than necessary)
+    panel_cols::StaticInteger{R} # number of columns of panels (can be larger than necessary)
 end
 
-"""
-Panel has column-major ordering of elements.
-"""
-struct Panel{T,V,M,N,Q,R,O,P} <: AbstractMatrix{T}
-    data::V # view on the actual memory
-    eltype::Type{T} # type of the elements
-    rows::StaticInteger{M} # rows of the view
-    cols::StaticInteger{N} # columns of the view
-    rows_per_panel::StaticInteger{Q} # rows in this panel (rows in memory)
-    cols_per_panel::StaticInteger{R} # columns in this panel (columns in memory)
-    offset_row::StaticInteger{O} # starting row of the view
-    offset_col::StaticInteger{P} # starting column of the view
-    function Panel(data::AbstractArray{T}, rows, cols, rows_per_panel, cols_per_panel,
-                         offset_row=0, offset_col=0) where T
-        @boundscheck length(data) >= rows_per_panel*cols_per_panel || error("Too little data!")
-        new{data, T, }() #TODO
+function PanelMatrix(data::AbstractMatrix{S}, rows, cols,
+                     offset_row=0, offset_col=0,
+                     panel_rows=cld(rows+offset_row, size(S,1)),
+                     panel_cols=cld(cols+offset_col, size(S,2)),
+                     ) where S
+    (rows_per_panel, cols_per_panel) = size(S)
+    @boundscheck begin
+        length(data) >= panel_rows*panel_cols || error("Too little data!") # TODO Create proper exceptopns as constants
+        offset_row + rows <= panel_rows*rows_per_panel || error("Too fews rows of panels")
+        offset_col + cols <= panel_cols*cols_per_panel || error("Too fews columns of panels")
+        all((rows, cols, offset_row, offset_col) .>= 0) || error("Negative sizes/offsets are not allowed.")
     end
+    return PanelMatrix(data, eltype(S), static(Int(rows)), static(Int(cols)), S,
+        static(Int(rows_per_panel)), static(Int(cols_per_panel)),
+        static(Int(offset_row)), static(Int(offset_col)),
+        static(Int(panel_rows)), static(Int(panel_cols)))
 end
 
+function PanelMatrix(x::AbstractMatrix, panel_type::Type=default_panel(eltype(x)))
+    (rows, cols) = size(x)
+    panel_rows=cld(rows, size(panel_type, 1))
+    panel_cols=cld(cols, size(panel_type, 2))
+    data = zeros(panel_type, (panel_rows, panel_cols)) # TODO: Cache line alignment
+    z = PanelMatrix(data, rows, cols)
+    #  TODO: implement z .= x
+    for i in eachindex(x)
+        z[i] = x[i]
+    end
+    return z
+end
 
-# TODO: We need paneling!
-getindex(x::Panel, i, j) = x.data[(j+x.offset_col)*x.cols_+(i+x.offset_row)*x.rows_stride]
+default_panel(T) = MMatrix{4, 4, T, 16}
+
+"""
+    getpanel(x, i, j)
+
+Get the panel containing element (i,j)
+"""
+function getpanel(x::PanelMatrix, i, j)
+    @boundscheck checkbounds(x, i, j)
+    # TODO inbounds
+    x.data[fld(i+x.offset_row, x.rows_per_panel)+1, fld(j+x.offset_col, x.cols_per_panel)+1]
+end
+
+Base.size(x::PanelMatrix) = (x.rows, x.cols)
+
+Base.eltype(x::PanelMatrix) = x.element_type
+
+function Base.setindex!(x::PanelMatrix, v, i, j)
+     getpanel(x, i, j)[(i % x.rows_per_panel) + 1 ,(j % x.cols_per_panel) + 1] = v
+end
+
+Base.getindex(x::PanelMatrix, i, j) = getpanel(x, i, j)[(i % x.rows_per_panel) + 1 ,(j % x.cols_per_panel) + 1]
 
 end # module
--- PanelMatrices/test/runtests.jl
+++ PanelMatrices/test/runtests.jl
@@ -1,6 +1,14 @@
 using PanelMatrices
 using Test
+using StaticArrays
+using StaticNumbers
 
 @testset "PanelMatrices.jl" begin
-    # Write your own tests here.
+    y = reshape(1:100, 10, 10)
+    x = PanelMatrix(y)
+    println(x)
+    println(x.data)
+    println(y)
+    @test x[1,1] == y[1,1]
+    @test all(x .== y)
 end
